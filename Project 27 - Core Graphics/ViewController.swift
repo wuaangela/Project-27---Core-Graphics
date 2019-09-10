b92f9841c837de35c0fa040711ba71186fbcf6cf
A W <AW@as-macbook-pro.local>
2019-09-10 13:14:53 -0400

    Initial Commit

---
+++ Project 27 - Core Graphics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 27 - Core Graphics
+//
+//  Created by A W on 2019-09-10.
+//  Copyright © 2019 A W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
